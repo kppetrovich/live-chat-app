0e63d67105cea8f0568edafb89883ff8024709bf
paulk24 <paulkur24@gmail.com>
2019-07-03 17:56:05 +0300

    Add Dashboard

--- live-chat/src/Dashboard.js
+++ live-chat/src/Dashboard.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -14,12 +20,14 @@ const useStyles = makeStyles(theme => ({
     },
     roomsWindow:{
         width: '30%',
-        height: '300px'
+        height: '300px',
+        borderRight: '1px solid grey'
     }
     ,
     chatWindow:{
         width: '70%',
-        height: '300px'
+        height: '300px',
+        padding: '20px'
     }
     ,
     chatBox:{
@@ -33,6 +41,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function Dashboard() {
     const classes = useStyles();
+    const [textValue, changeTextValue] = React.useState('')
     return(
         <div>
             <Paper className={classes.root}>
@@ -43,12 +52,45 @@ export default function Dashboard() {
                     Room placeholder
                 </Typography>
                 <div className={classes.flex}>
+                    <div className={classes.roomsWindow}>
+                        <List>
+                            {
+                                ['room'].map(rooms => (
+                                    <ListItem key={rooms} button>
+                                        <ListItemText primary={rooms} />
+                                    </ListItem>
+                                ))
+                            }
+                        </List>
+                    </div>
 
+                    <div className={classes.chatWindow}>
+                            {
+                                [{from: 'user', msg: 'hello'}].map((chat, i) => (
+                                   <div className={classes.flex} key={i}>
+                                       <Chip label={chat.from} className={classes.chip} />
+                                       <Typography>
+                                           {chat.msg}
+                                       </Typography>
+                                   </div>
+                                ))
+                            }
+                    </div>
                 </div>
                 <div className={classes.flex}>
+                    <TextField
+                        label="Send a message"
+                        className={classes.chatBox}
+                        value={textValue}
+                        onChange={e => changeTextValue(e.target.value)}
+                    />
+                    <Button variant="contained" color="primary" className={classes.button}>
+                        SEND
+                    </Button>
 
                 </div>
             </Paper>
         </div>
+
     )
 }
